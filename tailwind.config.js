72e8556fa7d68548c306fba1657b05d2dcefff8f
Testimony Benson <bensontestimony5@gmail.com>
2026-03-29 13:14:00 +0100

    Initial portfolio commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        obsidian: '#050505',
+        charcoal: '#121212',
+        gold: '#D4AF37',
+        silver: '#94A3B8',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
